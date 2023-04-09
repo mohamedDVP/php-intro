395cb1e0971cdddf7f7e07871896d4750981b7fc
mohamedDVP <mohamed.mak@laposte.net>
2023-04-09 15:15:29 +0200

    ajout de l'en-tête html dans le fichier index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php
-//Hello World
-echo "Hello World";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Studi</title>
+</head>
+<body>
+    <?php
+        //Hello World
+        echo "Hello World";
+    ?>
+</body>
+</html>
