4b0f754a3118f98b871e8099e4d0d4bfff317f09
mohamedDVP <mohamed.mak@laposte.net>
2023-04-10 15:30:17 +0200

    fichier intro.php pour l'introduction à php

---
+++ intro.php
@@ -0,0 +1,9 @@
+<?php
+// echo : fonction de la  SPL (Standard PHP Library)
+echo "Hello World !";
+
+// String
+// Int (nombre entier)
+// Float (nombre flottant ou à virgule)
+// Bool (booléen : true ou false)
+
