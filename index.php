878bc746ac15acd38582ed390c9efcca61e644ad
mohamedDVP <mohamed.mak@laposte.net>
2023-04-11 22:01:44 +0200

    vu : - les différentes façons de pouvoir incrémenter une variable à elle même - comment déclarer des constantes - mise en forme de la constante

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>PHP Studi</title>
 </head>
 <body>
@@ -11,6 +12,8 @@
         <?php echo "Hello Studi"; ?>
     </h1>
     <?php
+    //constante 
+        const SOFTWARE_VERSION = "1.0.0";
         // Le "=" est un opérateur d'affectation
         // Il fonctionne de droite à gauche
         $age = 38;
@@ -28,5 +31,13 @@
         $numero ++; //Equivalent : $numero +=1;
         echo "<br/>". $numero;
     ?>
+    <footer>
+        <p id="version">
+            Version : 
+            <?php
+                echo SOFTWARE_VERSION;
+            ?>
+        </p>
+    </footer>
 </body>
 </html>
