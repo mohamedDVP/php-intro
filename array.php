e7da5770ae6d8508d69a697df43ded56f075dae5
mohamedDVP <mohamed.mak@laposte.net>
2023-04-12 01:04:58 +0200

    mieux comprendre les tableaux

--- array.php
+++ array.php
@@ -1,3 +1,17 @@
 <?php
 // Array : tableau en PHP
+$array = [1, 2, 3, 4, 1];
+var_dump($array);
+array_unshift($array, "Coucou");
+//$array [] = "Coucou";
+var_dump($array);
+$array [10] = "Test";
+var_dump($array);
+
+//echo $array[5];
+
+//var_dump($array);
+echo "<br/>Hello ?";
+$monTableau = [... $array];
+var_dump($monTableau);
 ?>
