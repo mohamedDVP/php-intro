8da1d7d6c34be20db33c45ecba8a02305d81a1e9
mohamedDVP <mohamed.mak@laposte.net>
2023-04-23 12:15:32 +0200

    ajout du fichier list.php pour expliquer les tableaux multidimenssionnels

---
+++ list.php
@@ -0,0 +1,55 @@
+<?php
+
+// Liste d'utilisateurs
+// Utilisateur : id, name, age
+$users = [
+    [
+        "id" => 116,
+        "name" => "Blanche",
+        "age" => 99
+    ],
+    [
+        "id" => 119,
+        "name" => "Troy",
+        "age" => 33
+    ],
+    [
+        "id" => 11,
+        "name" => "Lula",
+        "age" => 43
+    ],
+    [
+        "id" => 118,
+        "name" => "Herbert",
+        "age" => 60
+    ],
+    [
+        "id" => 13,
+        "name" => "Lettie",
+        "age" => 11
+    ],
+    [
+        "id" => 112,
+        "name" => "Larry",
+        "age" => 53
+    ],
+    [
+        "id" => 14,
+        "name" => "Melvin",
+        "age" => 31
+    ]
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
