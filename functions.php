ce312db1d71795e8080ea9fcc04e256a10264146
mohamedDVP <mohamed.mak@laposte.net>
2023-04-30 22:07:03 +0200

    Ajout d'un footer et d'un header et personnalisation du titre de la page avec une condition. Création de la fonction isAgeLegal

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+// conversion dollar euro
+// fonction qui détermine si , pour un age donné , la personne est majeure
+
+function isAgeLegal(int $age): bool
+{
+    
+    return $age >= 18;
+
+    //return ($age >= 18) ? true : false
+
+    // if ($age >= 18)
+    // {
+    //     return true; // Je sors immédiatement de la fonction
+    // }
+
+    // return false;
+}
+
+$userAge = 15;
+
+//$age = 15; // On pourrait nommer la variable $age car elle n'a pas la même portée
+// if(isAgeLegal($userAge))
+// {
+//     echo "l'utilisateur est majeur";
+// }
+// else
+// {
+//     echo "l'utilisateur est mineur";
+// }
+
+echo isAgeLegal($userAge) ? "Majeur" : "Mineur";
--- list.php
+++ list.php
@@ -1,14 +1,9 @@
-<?php require_once 'data/users.php';?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste d'utilisateurs</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-</head>
-<body>
+<?php 
+require_once 'data/users.php';
+$title = 'Liste des utilisateurs';
+require_once 'layout/header.php';
+?>
+
     <!-- Conteneur général -->
     <div class="container">
         <!-- Elément conteneur de ma grille de cards -->
@@ -26,5 +21,5 @@
      <!-- Fin Elément conteneur de ma grille -->
     </div>
     <!-- Fin Conteneur général -->
-</body>
-</html>
+
+<?php  require_once 'layout/footer.php';?>
